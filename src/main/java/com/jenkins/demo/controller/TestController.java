cabb8de592af715028c9c4e0cade89e778bc7670
Uday <udp@narola.email>
2020-02-25 16:04:56 +0530

    initial commit

---
+++ src/main/java/com/jenkins/demo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+  @GetMapping("/one")
+  public String testOne(){
+    return "Hello I'm Jenkins Test One";
+  }
+
+}
