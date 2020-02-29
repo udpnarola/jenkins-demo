30cd410bf6a41e7c0a4edb0230f5d7bc65cda326
Uday <udp@narola.email>
2020-02-29 11:08:17 +0530

    test

--- src/main/java/com/jenkins/demo/controller/TestController.java
+++ src/main/java/com/jenkins/demo/controller/TestController.java
@@ -13,9 +13,9 @@ public class TestController {
     return "Hello I'm Jenkins Test One";
   }
 
-  /*@GetMapping("/two")
+  @GetMapping("/two")
   public String testTwo(){
     return "Hello I'm Jenkins Test Two";
-  }*/
+  }
 
 }
